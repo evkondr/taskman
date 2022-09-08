6061645e51632ac9d6a46b8a5ccf11824f65e62c
evkondr <kondratiev.evg@gmail.com>
2022-09-08 23:02:38 +0300

    feat: add pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,34 @@
 import React from 'react';
+import { Routes, Route } from "react-router-dom";
 import Header from './components/Header';
 import Navigation from './components/Navigation';
+import Container from './components/Container';
 import Logo from './components/Logo';
 import logourl from "./TASKMAN.png";
+import Home from './components/pages/Home';
+import Profile from './components/pages/Profile';
+import Users from './components/pages/Users';
+import Tasks from './components/pages/Tasks';
 
 function App() {
   return (
     <>
       <Header>
         <Logo>
-          <img src={logourl} alt="" />
+          <img src={logourl} alt="logo taskman" />
         </Logo>
         <Navigation /> 
       </Header>
+      <main>
+          <Container>
+            <Routes>
+                <Route path="/" element={<Home />}/>
+                <Route path="profile" element={<Profile />}/>
+                <Route path="users" element={<Users />}/>
+                <Route path="tasks" element={<Tasks />}/>
+              </Routes>
+          </Container>
+        </main>
     </>
   );
 }
---
+++ src/components/pages/Users.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styled from "styled-components";
+
+const Users = () => {
+    return <div>
+        Users
+    </div>
+}
+export default Users;
