2cd80e2a158384ae27811c666ff91dd9c86b56ec
Кондратьев Евгений Александрович <RUCO2550@ridancorp.net>
2022-09-20 23:03:55 +0300

    refator: fix error handling in asyncActions

--- src/Store/asyncActions.ts
+++ src/Store/asyncActions.ts
@@ -1,4 +1,5 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios, { AxiosError } from "axios";
 import { ITodo, IUser } from "../interfaces";
 import todosService from "../services/todosSetvice";
 import userService from "../services/userService";
@@ -7,16 +8,26 @@ export const fetchUsers = createAsyncThunk<IUser[]>("users/fetch", async(_, {rej
     try {
         const response = await userService.getAll()
         return response.data;
-    } catch (error:any) {
-        return rejectWithValue(error.message)
+    } catch (error) {
+        if(axios.isAxiosError(error)){
+            console.log(error)
+            return rejectWithValue(error.message);
+        }else{
+            return rejectWithValue(error)
+        }
     }
 })
 export const fetchUserById = createAsyncThunk<IUser, string>("user/fetchById", async(id, {rejectWithValue}) => {
     try {
         const response = await userService.getUserById(id);
         return response.data;
-    } catch (error:any) {
-        return rejectWithValue(error.message)
+    } catch (error) {
+        if(axios.isAxiosError(error)){
+            console.log(error)
+            return rejectWithValue(error.message);
+        }else{
+            return rejectWithValue(error)
+        }
     }
 })
 
@@ -25,7 +36,12 @@ export const fetchTodosByUserId = createAsyncThunk<ITodo[], string>("todos/fetch
     try {
         const response = await todosService.getTodosByUserId(id);
         return response.data;
-    } catch (error:any) {
-        return rejectWithValue(error.message)
+    } catch (error) {
+        if(axios.isAxiosError(error)){
+            console.log(error)
+            return rejectWithValue(error.message);
+        }else{
+            return rejectWithValue(error)
+        }
     }
 })
--- src/utils.tsx
+++ src/utils.tsx
@@ -1,4 +1,5 @@
 import { AnyAction } from "@reduxjs/toolkit"
+import axios from "axios";
 
 export const isError = (action:AnyAction) => {
     return action.type.endsWith("rejected");
