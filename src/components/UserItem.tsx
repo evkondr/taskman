7bf39e8da83486b14a86944272dac8b1875de8c8
evkondr <kondratiev.evg@gmail.com>
2022-12-31 22:33:15 +0300

    refactor: refactor user items style

--- src/components/UserItem.tsx
+++ src/components/UserItem.tsx
@@ -10,6 +10,7 @@ const StyledItem = styled.div`
     flex-direction: column;
     justify-content: flex-start;
     border: 1px solid rgba(0,0,0, .1);
+    border-radius: 5px;
     height: 230px;
     font-size: 1.5rem;
     box-shadow: 1px 1px 5px rgba(0,0,0, .5);
@@ -22,12 +23,15 @@ const StyledItem = styled.div`
         transform: scale(110%);
         box-shadow: 10px 10px 5px rgba(0,0,0, .5);;
     }
+    h5{
+        margin-bottom: 20px;
+    }
 `
 
 const UserItem = (props:IUserItem) => {
     const {user} = props
     return <StyledItem>
-        <p>{user.name}</p>
+        <h5>{user.name}</h5>
         <Avatar />
     </StyledItem>
 }
